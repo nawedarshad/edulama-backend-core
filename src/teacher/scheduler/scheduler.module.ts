9802b3187e28c97fa1b614ee7ded497daaf98d45
craftr <nawedarshad25@gmail.com>
2026-02-06 18:34:08 +0530

    Lesson

--- src/teacher/scheduler/scheduler.module.ts
+++ src/teacher/scheduler/scheduler.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { SchedulerController } from './scheduler.controller';
 import { SchedulerService } from './scheduler.service';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
+    imports: [HttpModule, ConfigModule],
     controllers: [SchedulerController],
     providers: [SchedulerService, PrismaService],
 })
