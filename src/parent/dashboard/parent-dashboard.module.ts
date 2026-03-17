2307d480dfc6e0314adfafd851c837f1b5e646aa
craftr <nawedarshad25@gmail.com>
2026-03-17 13:02:36 +0530

    K

--- src/parent/dashboard/parent-dashboard.module.ts
+++ src/parent/dashboard/parent-dashboard.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { ParentDashboardController } from './parent-dashboard.controller';
 import { ParentDashboardService } from './parent-dashboard.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+    imports: [
+        HttpModule,
+        ConfigModule,
+        PrismaModule,
+    ],
     controllers: [ParentDashboardController],
     providers: [ParentDashboardService],
     exports: [ParentDashboardService],
